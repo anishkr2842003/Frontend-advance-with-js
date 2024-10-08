7b1d6170621789587ec729864a2ac75348ddaa05
Anish kumar <anishkr2842003@gmail.com>
2024-10-08 16:09:50 +0530

    first commit

---
+++ responsive day 1/script.js
@@ -0,0 +1,44 @@
+
+// let const - {es - 6}
+
+// var - old js
+
+// var const let
+
+// int a = 9;
+// string abc = "a"
+
+// var a = 9
+// let b = 9
+
+// var name = 'a'
+
+// const DBURI = "loremisksjdnv87jhb"
+
+// string 
+
+// var num = 5;
+
+// 'abc'
+
+// "abc"
+
+// `abc ${num}` 
+
+
+var age = [15,18,27,28]
+
+var name = ['a','b','c','d']
+
+var abc = 
+[18,19,20,'a','c','d', function abc(){},{name:'anish'}, [5,6,['a',b]]]
+
+// backend 10 category - name - a
+// category - a - b - 
+
+
+// obj -  
+
+// foreach - array obj nested arra obj json.
+
+// fetch
