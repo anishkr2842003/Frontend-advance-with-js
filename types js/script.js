da998c204c681a4e2414f3cbb0e19accaa09f7ab
Anish kumar <anishkr2842003@gmail.com>
2024-10-11 15:01:14 +0530

    day - 3 typed js

---
+++ types js/script.js
@@ -0,0 +1,16 @@
+var typed = new Typed('.element', {
+    strings: [
+        "Good Night", 
+        "Good morning", 
+        "Good afternoon"
+    ],
+    typeSpeed: 30, // likhane ki speed
+    startDelay: 2000, // kitne time baad animation start hoga
+    backSpeed: 1000, // kitne speed me text ko delete karega
+    smartBackspace: false,
+    // fadeOut: true,
+    loop: true, // bina ruke chalte rahega
+    cursorChar: '!', // cursor badl sakte ho
+    shuffle: true, 
+  });
+  
